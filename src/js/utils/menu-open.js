d71a56473ffca72bf711a155edd624e408b7809e
Artur <a.dymchevskyi@student.bachelor-goit.com.ua>
2026-04-16 12:31:47 -0400

    fix hero section and js scripts + add images

--- src/js/utils/menu-open.js
+++ src/js/utils/menu-open.js
@@ -1,30 +1,30 @@
 export function initMenu() {
   const menuOpenBtn = document.querySelector('.menu-open_btn');
-  const MenuContainer = document.querySelector('.menu-container');
+  const menuContainer = document.querySelector('.menu-container');
   const menuCloseBtn = document.querySelector('.menu-close_btn');
   const mainBody = document.querySelector('body');
   const navLinks = document.querySelectorAll('.nav-link');
   const menuBtnMobile = document.querySelector('.nav-btn--mobile');
 
   menuCloseBtn.addEventListener('click', () => {
-    MenuContainer.classList.remove('is-active');
+    menuContainer.classList.remove('is-active');
     mainBody.classList.remove('no-scroll');
   });
 
   menuOpenBtn.addEventListener('click', () => {
-    MenuContainer.classList.toggle('is-active');
+    menuContainer.classList.toggle('is-active');
     mainBody.classList.toggle('no-scroll');
   });
 
   navLinks.forEach(link => {
     link.addEventListener('click', () => {
-      MenuContainer.classList.remove('is-active');
+      menuContainer.classList.remove('is-active');
       mainBody.classList.remove('no-scroll');
     });
   });
 
   menuBtnMobile.addEventListener('click', () => {
-    MenuContainer.classList.remove('is-active');
+    menuContainer.classList.remove('is-active');
     mainBody.classList.remove('no-scroll');
   });
 }
--- src/js/utils/modal-open.js
+++ src/js/utils/modal-open.js
@@ -1,29 +1,34 @@
 export function initModal() {
   const orderOpenBtn = document.querySelector('.product-modal_btn');
-  const ProductCloseBtn = document.querySelector('.product-close_btn');
-  const ProductBackground = document.querySelector('.product-background');
+  const productCloseBtn = document.querySelector('.product-close_btn');
+  const productBackground = document.querySelector('.product-background');
   const productOpenBtn = document.querySelectorAll('.product-open');
   const orderCloseBtn = document.querySelector('.order-close_btn');
   const orderModalBackground = document.querySelector('.order-background');
-  if (productOpenBtn && ProductCloseBtn && ProductBackground) {
+  const mainBody = document.querySelector('body');
+  if (productOpenBtn && productCloseBtn && productBackground) {
     productOpenBtn.forEach(btn => {
       btn.addEventListener('click', () => {
-        ProductBackground.classList.add('active');
+        productBackground.classList.add('active');
+        mainBody.classList.add('no-scroll');
       });
     });
 
-    ProductCloseBtn.addEventListener('click', () => {
-      ProductBackground.classList.remove('active');
+    productCloseBtn.addEventListener('click', () => {
+      productBackground.classList.remove('active');
+      mainBody.classList.remove('no-scroll');
     });
   }
   if (orderOpenBtn && orderCloseBtn && orderModalBackground) {
     orderOpenBtn.addEventListener('click', () => {
       orderModalBackground.classList.add('active');
-      ProductBackground.classList.remove('active');
+      productBackground.classList.remove('active');
+      mainBody.classList.add('no-scroll');
     });
 
     orderCloseBtn.addEventListener('click', () => {
       orderModalBackground.classList.remove('active');
+      mainBody.classList.remove('no-scroll');
     });
   }
 }
